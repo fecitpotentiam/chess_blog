511175aa13557d85fc10ec04d13e858acecf49b5
oksana <oksanamushtak@gmail.com>
2021-02-10 13:06:39 +0500

    Added photo handler

--- chess_coach_blog/urls.py
+++ chess_coach_blog/urls.py
@@ -22,11 +22,18 @@ from .settings import MEDIA_URL, MEDIA_ROOT
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('event/<int:pk>/', views.event_detail, name='event_detail'),
     path('blog/category/<int:pk>/', views.category_post_list, name='category_post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+
     path('events', views.events_list, name='events_list'),
-    path('image_gallery', views.image_gallery, name='image_gallery'),
+    path('event/<int:pk>/', views.event_detail, name='event_detail'),
+
+    path('albums', views.get_albums, name='get_albums'),
+    path('album/<int:pk>', views.get_photos_in_album, name='get_photos_in_album'),
+
+    path('videolessons', views.youtube_videos, name='youtube_videos'),
+
     path('tinymce/', include('tinymce.urls')),
-    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post')
+
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
