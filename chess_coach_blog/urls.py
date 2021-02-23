93842d6034ad7ece9a7c1298230363e5433cf568
fecitpotentiam <seeingyouinhell@gmail.com>
2021-02-23 18:19:18 +0500

    Renames VideoLessons to Video

--- chess_coach_blog/urls.py
+++ chess_coach_blog/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
     path('qa', views.question_answer, name='qa'),
     path('thanks', views.thanks, name='thanks'),
 
-    path('videolessons', views.youtube_videos, name='youtube_videos'),
+    path('video', views.youtube_videos, name='youtube_videos'),
     path('book', views.book, name='book'),
     path('contacts', views.contacts, name='contacts'),
 
